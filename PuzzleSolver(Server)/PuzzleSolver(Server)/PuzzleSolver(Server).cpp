88d9840be881c8feb5fc4ba957b716f35cb3106c
Gali Kertser <gali.kertser@gmail.com>
2022-11-15 17:08:47 +0200

    The buetiful GrayScale is done

--- PuzzleSolver(Server)/PuzzleSolver(Server)/PuzzleSolver(Server).cpp
+++ PuzzleSolver(Server)/PuzzleSolver(Server)/PuzzleSolver(Server).cpp
@@ -3,6 +3,5 @@
 int main()
 {
     Picture picture("C:\\Users\\magshimim\\Desktop\\PuzzleSolver\\puzzle-maker\\Pictures\\PictureA.jpg");
-    picture.ResizeCanvas();
     picture.ShowPicture();  
 }
